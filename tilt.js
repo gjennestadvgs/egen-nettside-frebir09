37b7c23b6f86dd6e2b0118b54ef1cc13850339e8
Fredrik Birkely <frebir09@gvgs.no>
2025-09-24 23:34:27 +0200

    Nå kan du (helst ikke) kontakte meg, tilt, mer javascript, typewrite

---
+++ tilt.js
@@ -0,0 +1,16 @@
+document.querySelectorAll('.Website_Card, .Project, .Tilt_Img').forEach(card => {
+  card.addEventListener('mousemove', function(e) {
+    const rect = card.getBoundingClientRect();
+    const x = e.clientX - rect.left;
+    const y = e.clientY - rect.top;
+    const centerX = rect.width / 2;
+    const centerY = rect.height / 2;
+    const rotateX = ((centerY - y) / centerY) * 10;
+    const rotateY = ((centerX - x) / centerX) * -10;
+    card.style.transform = `perspective(600px) rotateX(${rotateX}deg) rotateY(${rotateY}deg) scale(1.05)`;
+  });
+
+  card.addEventListener('mouseleave', function() {
+    card.style.transform = 'perspective(600px) scale(1)';
+  });
+});
